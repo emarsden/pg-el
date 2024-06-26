d5e0ebf603227147f19fded517b0de5e8660ebf9
Eric Marsden <eric.marsden@risk-engineering.org>
2024-06-26 16:28:52 +0200

    Split out geometry and large object support into separate files

--- pg.el
+++ pg.el
@@ -84,6 +84,7 @@
 (require 'url)
 (require 'peg)
 
+
 (defvar pg-application-name (or (getenv "PGAPPNAME") "pg.el")
   "The application_name sent to the PostgreSQL backend.
 This information appears in queries to the `pg_stat_activity' table
@@ -129,7 +130,7 @@ struct.")
 (defvar pg--serializers (make-hash-table :test #'equal))
 
 ;; Contains an entry for types that serialize to a text format, rather than a binary format (e.g.
-;; HSTORE).
+;; HSTORE). The serialization function itself is stored in pg--serializers.
 (defvar pg--textual-serializers (make-hash-table :test #'equal))
 
 ;; Maps from type-name to a parsing function (from string to Emacs native type). This is built
@@ -1001,8 +1002,7 @@ PostgreSQL type names of the form (\"int4\" \"text\" \"bool\")."
               ;; will be sending this type in binary form: return that OID. Otherwise, return the
               ;; pseudo-OID value of 0 to tell PostgreSQL that we are sending this type in text
               ;; format.
-              (if (or (gethash type-name pg--serializers)
-                      (gethash type-name pg--textual-serializers))
+              (if (gethash type-name pg--serializers)
                   (or (pg--lookup-oid con type-name)
                       (signal 'pg-error
                               (list (format "Don't know the OID for PostgreSQL type %s" type-name))))
@@ -1658,6 +1658,15 @@ PostgreSQL and Emacs. CON should no longer be used."
 ;; Some databases such as Clickhouse that implement the PostgreSQL wire protocol do implement the
 ;; pg_type table. They send all data in textual format with an OID of zero. For this reason, we
 ;; tolerate an error in the query on pg_type and leave all our oid-related caches empty.
+;;
+;; Note: the psycopg libary make the following query to also retrieve datatype delimiters and array
+;; types:
+;;
+;; SELECT typname AS name, oid, typarray AS array_oid, oid::regtype::text AS regtype, typdelim AS delimiter
+;; FROM pg_type t
+;; WHERE t.oid = to_regtype($1)
+;; ORDER BY t.oid
+
 (defun pg-initialize-parsers (con)
   "Initialize the datatype parsers on PostgreSQL connection CON."
   (clrhash pg--parser-by-oid)
@@ -1830,7 +1839,7 @@ Return nil if the extension could not be loaded."
       (lambda (ht)
         (cl-assert (hash-table-p ht))
         (let ((kv (list)))
-          ;; FIXME need to escape \" characters in k and v
+          ;; FIXME should escape \" characters in k and v
           (maphash (lambda (k v) (push (format "\"%s\"=>\"%s\"" k v) kv)) ht)
           (string-join kv ","))))))
 
@@ -2197,9 +2206,11 @@ Return nil if the extension could not be set up."
     (bindat-pack (bindat-type uint 64 nil) v)))
 
 ;; We send floats in text format, because we don't know how to access the binary representation from
-;; Emacs Lisp. Here a possible conversion routine, but there is probably no performance benefit to
-;; using it.
+;; Emacs Lisp. Here a possible conversion routine and reader, but there is probably no performance
+;; benefit to using it.
+;;
 ;; https://lists.gnu.org/archive/html/help-gnu-emacs/2002-10/msg00724.html
+;; https://www.emacswiki.org/emacs/read-float.el
 (defun pg--serialize-float (number)
   "Serialize floating point NUMBER to PostgreSQL wire-level text format for floats.
 Respects floating-point infinities and NaN."
@@ -2213,9 +2224,12 @@ Respects floating-point infinities and NaN."
 (pg-register-textual-serializer "float8" #'pg--serialize-float)
 
 (if (fboundp 'json-serialize)
-    (pg-register-textual-serializer "json" #'json-serialize)
+    (progn
+      (pg-register-textual-serializer "json" #'json-serialize)
+      (pg-register-textual-serializer "jsonb" #'json-serialize))
   (require 'json)
-  (pg-register-textual-serializer "json" #'json-encode))
+  (pg-register-textual-serializer "json" #'json-encode)
+  (pg-register-textual-serializer "jsonb" #'json-encode))
 
 (defun pg--serialize-encoded-time-date (encoded-time)
   (cl-assert (listp encoded-time))
@@ -2470,194 +2484,6 @@ Uses database connection CON."
   (let* ((sql "SELECT * FROM pg_proc WHERE proname = $1")
          (res (pg-exec-prepared con sql `((,name . "text")))))
     (pg-result res :tuples)))
-
-
-;; large object support ================================================
-;;
-;; Humphrey: Who is Large and to what does he object?
-;;
-;; Large objects are the PostgreSQL way of doing what most databases
-;; call BLOBs (binary large objects). In addition to being able to
-;; stream data to and from large objects, PostgreSQL's
-;; object-relational capabilities allow the user to provide functions
-;; which act on the objects.
-;;
-;; For example, the user can define a new type called "circle", and
-;; define a C or Tcl function called `circumference' which will act on
-;; circles. There is also an inheritance mechanism in PostgreSQL.
-;;
-;;======================================================================
-(defconst pg--INV_ARCHIVE 65536)         ; fe-lobj.c
-(defconst pg--INV_WRITE   131072)
-(defconst pg--INV_READ    262144)
-(defconst pg--LO_BUFIZE   1024)
-
-(defvar pg-lo-initialized nil)
-(defvar pg-lo-functions '())
-
-(defun pg-lo-init (con)
-  (let* ((res (pg-exec con
-                       "SELECT proname, oid from pg_proc WHERE "
-                       "proname = 'lo_open' OR "
-                       "proname = 'lo_close' OR "
-                       "proname = 'lo_creat' OR "
-                       "proname = 'lo_unlink' OR "
-                       "proname = 'lo_lseek' OR "
-                       "proname = 'lo_tell' OR "
-                       "proname = 'loread' OR "
-                       "proname = 'lowrite'")))
-    (setq pg-lo-functions '())
-    (mapc
-     (lambda (tuple)
-       (push (cons (car tuple) (cadr tuple)) pg-lo-functions))
-     (pg-result res :tuples))
-    (setq pg-lo-initialized t)))
-
-;; fn is either an integer, in which case it is the OID of an element
-;; in the pg_proc table, and otherwise it is a string which we look up
-;; in the alist `pg-lo-functions' to find the corresponding OID.
-(defun pg-fn (con fn integer-result &rest args)
-  (pg-connection-set-busy con t)
-  (unless pg-lo-initialized
-    (pg-lo-init con))
-  (let ((fnid (cond ((integerp fn) fn)
-                    ((not (stringp fn))
-                     (let ((msg (format "Expecting a string or an integer: %s" fn)))
-                       (signal 'pg-protocol-error (list msg))))
-                    ((assoc fn pg-lo-functions) ; blech
-                     (cdr (assoc fn pg-lo-functions)))
-                    (t
-                     (error "Unknown builtin function %s" fn)))))
-    (pg-send-char con ?F)
-    (pg-send-char con 0)
-    (pg-send-uint con fnid 4)
-    (pg-send-uint con (length args) 4)
-    (mapc (lambda (arg)
-            (cond ((integerp arg)
-                   (pg-send-uint con 4 4)
-                   (pg-send-uint con arg 4))
-                  ((stringp arg)
-                   (pg-send-uint con (length arg) 4)
-                   (pg-send con arg))
-                  (t
-                   (error "Unknown fastpath type %s" arg))))
-          args)
-    (pg-flush con)
-    (cl-loop with result = (list)
-          for c = (pg-read-char con) do
-          (cl-case c
-             ;; ErrorResponse
-            (?E (pg-handle-error-response con "in pg-fn"))
-
-            ;; FunctionResultResponse
-            (?V (setq result t))
-
-            ;; Nonempty response
-            (?G
-             (let* ((len (pg-read-net-int con 4))
-                    (res (if integer-result
-                             (pg-read-net-int con len)
-                           (pg-read-chars con len))))
-               (setq result res)))
-
-            ;; NoticeResponse
-            (?N
-             (let ((notice (pg-read-string con pg--MAX_MESSAGE_LEN)))
-               (message "NOTICE: %s" notice))
-             (unix-sync))
-
-            ;; ReadyForQuery message
-            (?Z
-             (let ((_msglen (pg-read-net-int con 4))
-                   (status (pg-read-char con)))
-               (when (eql ?E status)
-                 (message "PostgreSQL ReadyForQuery message with error status"))
-               (pg-connection-set-busy con nil)
-               (cl-return-from pg-fn result)))
-
-            ;; end of FunctionResult
-            (?0 nil)
-
-            (t (error "Unexpected character in pg-fn: %s" c))))))
-
-;; returns an OID
-(defun pg-lo-create (connection &optional args)
-  (let* ((modestr (or args "r"))
-         (mode (cond ((integerp modestr) modestr)
-                     ((string= "r" modestr) pg--INV_READ)
-                     ((string= "w" modestr) pg--INV_WRITE)
-                     ((string= "rw" modestr)
-                      (logior pg--INV_READ pg--INV_WRITE))
-                     (t (error "pg-lo-create: bad mode %s" modestr))))
-         (oid (pg-fn connection "lo_creat" t mode)))
-    (cond ((not (integerp oid))
-           (error "Returned value not an OID: %s" oid))
-          ((zerop oid)
-           (error "Can't create large object"))
-          (t oid))))
-
-;; args = modestring (default "r", or "w" or "rw")
-;; returns a file descriptor for use in later pg-lo-* procedures
-(defun pg-lo-open (connection oid &optional args)
-  (let* ((modestr (or args "r"))
-         (mode (cond ((integerp modestr) modestr)
-                     ((string= "r" modestr) pg--INV_READ)
-                     ((string= "w" modestr) pg--INV_WRITE)
-                     ((string= "rw" modestr)
-                      (logior pg--INV_READ pg--INV_WRITE))
-                     (t (error "pg-lo-open: bad mode %s" modestr))))
-         (fd (pg-fn connection "lo_open" t oid mode)))
-    (unless (integerp fd)
-      (error "Couldn't open large object"))
-    fd))
-
-(defsubst pg-lo-close (connection fd)
-  (pg-fn connection "lo_close" t fd))
-
-(defsubst pg-lo-read (connection fd bytes)
-  (pg-fn connection "loread" nil fd bytes))
-
-(defsubst pg-lo-write (connection fd buf)
-  (pg-fn connection "lowrite" t fd buf))
-
-(defsubst pg-lo-lseek (connection fd offset whence)
-  (pg-fn connection "lo_lseek" t fd offset whence))
-
-(defsubst pg-lo-tell (connection oid)
-  (pg-fn connection "lo_tell" t oid))
-
-(defsubst pg-lo-unlink (connection oid)
-  (pg-fn connection "lo_unlink" t oid))
-
-;; returns an OID
-;; FIXME should use unwind-protect here
-(defun pg-lo-import (connection filename)
-  (let* ((buf (get-buffer-create (format " *pg-%s" filename)))
-         (oid (pg-lo-create connection "rw"))
-         (fdout (pg-lo-open connection oid "w"))
-         (pos (point-min)))
-    (with-current-buffer buf
-      (insert-file-contents-literally filename)
-      (while (< pos (point-max))
-        (pg-lo-write
-         connection fdout
-         (buffer-substring-no-properties pos (min (point-max) (cl-incf pos 1024)))))
-      (pg-lo-close connection fdout))
-    (kill-buffer buf)
-    oid))
-
-(defun pg-lo-export (connection oid filename)
-  (let* ((buf (get-buffer-create (format " *pg-%d" oid)))
-         (fdin (pg-lo-open connection oid "r")))
-    (with-current-buffer buf
-      (cl-do ((str (pg-lo-read connection fdin 1024)
-                   (pg-lo-read connection fdin 1024)))
-          ((or (not str)
-               (zerop (length str))))
-        (insert str))
-      (pg-lo-close connection fdin)
-      (write-file filename))
-    (kill-buffer buf)))
 
 
 
@@ -3023,6 +2849,9 @@ presented to the user."
                 (when p
                   (kill-process p)))
            (kill-buffer buffer)))
+
+(require 'pg-geometry)
+(require 'pg-lo)
 
 
 (provide 'pg)
