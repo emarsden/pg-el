91405b00575645a7b85e66264fd000d729f177fe
Eric Marsden <eric.marsden@risk-engineering.org>
2024-06-27 19:05:39 +0200

    Add support for LSEG geometric type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@
 
 - Fix serialization of JSONB parameters in prepared statements.
 
-- Preliminary support for the PostgreSQL POINT and LINE types (geometric types).
+- Preliminary support for the PostgreSQL POINT, LINE and LSEG types (geometric types).
 
 
 ## [0.36] - 2024-06-23
--- pg-geometry.el
+++ pg-geometry.el
@@ -52,12 +52,42 @@
   (cl-assert (numberp (aref line 2)))
   (format "{%f,%f,%f}" (aref line 0) (aref line 1) (aref line 2)))
 
+;; An lseg is represented in Emacs Lisp by a two-element vector of points.
+(defun pg--lseg-parser (str _encoding)
+  (message "Parsing lseg %s" str)
+  (with-temp-buffer
+    (insert str)
+    (goto-char (point-min))
+    (with-peg-rules
+     ((lseg (* [space]) "[" point "," (* [space]) point (* [space]) "]" (* [space]) (eol)
+            `(p1 p2 -- (vector p1 p2)))
+      (point "(" x-comma-y ")")
+      (x-comma-y (* [space]) float (* [space]) ","
+                 (* [space]) float (* [space])
+                 `(x y -- (cons x y)))
+      (float (substring sign (+ [digit]) (* "." (+ [digit])) (* "e" sign (+ [digit])))
+             `(str -- (string-to-number str)))
+      (sign (or "+" "-" "")))
+     (car (peg-run (peg lseg))))))
+
+;; [(x1,y1),(x2,y2)]
+(defun pg--serialize-lseg (lseg)
+  (cl-assert (vectorp lseg))
+  (cl-assert (eql 2 (length lseg)))
+  (format "[(%f,%f),(%f,%f)]"
+          (car (aref lseg 0))
+          (cdr (aref lseg 0))
+          (car (aref lseg 1))
+          (cdr (aref lseg 1))))
+
 (eval-after-load 'pg
   (progn
     (pg-register-parser "point" #'pg--point-parser)
     (pg-register-textual-serializer "point" #'pg--serialize-point)
     (pg-register-parser "line" #'pg--line-parser)
-    (pg-register-textual-serializer "line" #'pg--serialize-line)))
+    (pg-register-textual-serializer "line" #'pg--serialize-line)
+    (pg-register-parser "lseg" #'pg--lseg-parser)
+    (pg-register-textual-serializer "lseg" #'pg--serialize-lseg)))
 
 
 (provide 'pg-geometry)
