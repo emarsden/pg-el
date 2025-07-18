cbdf60551670035074658ec3a56d3bf574ba530d
Eric Marsden <eric.marsden@risk-engineering.org>
2025-07-18 11:16:24 +0200

    Implement fixes from Stefan Monnier

--- test/install-deps.el
+++ test/install-deps.el
@@ -2,15 +2,14 @@
 
 (require 'cl-lib)
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 (progn
   (setq package-user-dir (getenv "DEPS_DIR"))
   (package-refresh-contents)
-  (if-let ((reqs (package-desc-reqs (package-buffer-info)))
-           (transaction (package-compute-transaction nil reqs)))
+  (if-let* ((reqs (package-desc-reqs (package-buffer-info)))
+            (transaction (package-compute-transaction nil reqs)))
       (progn
         (message "Installing %s..."
-                 (mapconcat (quote package-desc-full-name) transaction ", "))
+                 (mapconcat #'package-desc-full-name transaction ", "))
         (package-download-transaction transaction))
     (message "Nothing to install")))
